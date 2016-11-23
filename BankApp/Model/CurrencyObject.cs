03f45c6e21fd87acf340f25579ec090903a6c8aa
Triston Tan <ttan08@hotmail.com>
2016-11-23 16:43:06 +1300

    Initial

---
+++ BankApp/Model/CurrencyObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BankApp.Model
+{
+    public class CurrencyObject
+    {
+        public string base_curr { get; set; }
+        public string date { get; set; }
+        public int AUD { get; set; }
+        public int GBP { get; set; }
+        public int EUR { get; set; }
+        public int JPY { get; set; }
+        public int USD { get; set; }
+    }
+}
