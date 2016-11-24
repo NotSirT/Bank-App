c337186e6f4e0a78b3c9816a2a8084960cb20973
Triston Tan <ttan08@hotmail.com>
2016-11-24 21:58:14 +1300

    Commit Message

--- BankApp/Controllers/MessagesController.cs
+++ BankApp/Controllers/MessagesController.cs
@@ -37,9 +37,60 @@ namespace BankApp
                 // message output is set to this on default 
                 string endOutput = "Hello, welcome to Contoso, \n\n type 'help' to return a list of our features";
 
-                
 
-                if (userMessage.ToLower().Equals("branches"))
+                // help info
+                if (userMessage.ToLower().Equals("help"))
+                {
+                    endOutput = "";
+                    endOutput += "This is our list of features: \n\n 'branches' - shows a list of all our branches \n\n 'staff' - shows a list of all our staff members \n\n 'exchange rates' - shows popular currency exchange rates against our NZD \n\n 'show branch [branch name]' - shows more details of the specific branch \n\n 'show staff [staff name]' - shows more details of the specific staff member";
+                }
+
+                //to request more info of specific staff members
+                if (userMessage.Length > 11)
+                {
+                    if (userMessage.ToLower().Substring(0, 10).Equals("show staff"))
+                    {
+                        string msgstaff = userMessage.ToLower().Substring(11);
+                        endOutput = msgstaff;
+                        List<Staff> staff= await AzureManager.AzureManagerInstance.GetStaff();
+                        endOutput = "";
+                        int count = 0;
+                        foreach (Staff t in staff)
+                        {
+                            if (msgstaff == t.Name.ToLower())
+                            {
+                                endOutput += "Staff Information: \n\n Name: " + t.Name;
+                                endOutput += "\n\n Branch: " + t.Branch + "\n\n Position: " + t.Position + "\n\n Contact Number: " + t.Contact_no + "\n\n Contact Email: " + t.Contact_email;
+                            }
+                        }
+                    }
+                }
+                //to request more info of specific branches
+                if (userMessage.Length > 12)
+                {
+                    if (userMessage.ToLower().Substring(0, 11).Equals("show branch"))
+                    {
+                        string msgbranch = userMessage.ToLower().Substring(12);
+                        endOutput = msgbranch;
+                        List<Branch_Tables> branch = await AzureManager.AzureManagerInstance.GetBranch();
+                        endOutput = "";
+                        int count = 0;
+                        foreach (Branch_Tables t in branch)
+                        {
+                            if (msgbranch == t.Location.ToLower())
+                            {
+                                endOutput += "Bank Info: \n\n Location: " + t.Location;
+                                endOutput += "\n\n Opening Hours: \n\n Monday:" + t.Monday + "\n\n Tuesday: " + t.Tuesday + "\n\n Wednesday: " + t.Wednesday + "\n\n Thursday: " + t.Thursday + "\n\n Friday: " + t.Friday + "\n\n Saturday: " + t.Saturday + "\n\n Sunday: " + t.Sunday;
+                                endOutput += "\n\n 24/7 Deposit: " + t._24_7_deposit + "\n\n Coin Deposit: " + t.Coin_deposit;
+
+                            }
+                        }
+                    }
+                }
+
+
+                    // checks the messsage to see if it equals "branches" if so, return the location of all branches in the DB
+                    if (userMessage.ToLower().Equals("branches"))
                 {
                     List<Branch_Tables> branch = await AzureManager.AzureManagerInstance.GetBranch();
                     endOutput = "";
@@ -47,8 +98,9 @@ namespace BankApp
                     {
                         endOutput += "Location: " + t.Location + "\n\n";
                     }
-
                 }
+
+                if (userMessage.ToLower().Equals("branches"))
 
                 //checks if message is "exchange rates" and then returns the exchange rates against the NZD in a card
                 if (userMessage.ToLower().Equals("exchange rates"))
@@ -76,7 +128,7 @@ namespace BankApp
                     ThumbnailCard plCard = new ThumbnailCard()
                     {
                         Title = "The exchange rate for NZD: ",
-                        Subtitle = ($"AUD: {AUD}          \n\n GBP: {GBP} \n\n EUR: {EUR} \n\n  JPY:{JPY} \n\n USD: {USD}"),
+                        Subtitle = ($"AUD: {AUD} \n\n GBP: {GBP} \n\n EUR: {EUR} \n\n  JPY:{JPY} \n\n USD: {USD}"),
                         Images = cardImages,
                         Buttons = cardButtons
                     };
@@ -88,8 +140,6 @@ namespace BankApp
                     return Request.CreateResponse(HttpStatusCode.OK);
 
                 }
-
-                /*------------------------------*/
 
                 //checks if message is "staff" and then returns the staff in the database
 
